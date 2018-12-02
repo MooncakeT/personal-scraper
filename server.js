12d48e3d14f56275d38748c7b18805b932c45869
MooncakeT <43077353+MooncakeT@users.noreply.github.com>
2018-12-02 14:39:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,42 @@ app.get('/', (req, res) => {
 });
 
 // scraper endpoint
+app.get('/dragon/:keyword', (req, res) => {
+  var keyword = req.params.keyword;
+
+//define function to find dragon pictures
+  function findDragonImage(keyword) { //defines function to find dragon pictures
+    var nightmare = Nightmare({show: true}); //shows bot searching or not
+
+    return nightmare
+      .goto('https://www.google.com') //goes to google
+      .insert('input[title="Search"]', `dragon ${keyword}`) //puts the search term (dragon + keyword) into the search box
+      .click('input[value="Google Search"]') //clicks the seach button to search for term
+      .wait('a.q.qs') //waits for the link to appear
+      .click('a.q.qs') //clicks the "images" link
+      .wait('div#res.med') //waits for div to appear (container for images)
+      .evaluate(function() { //gets images from google & sends back their sources
+        var photoDivs = document.querySelectorAll('img.rg_ic');
+        var list = [].slice.call(photoDivs); //creates a list for photos
+
+        return list.map(function(div) {
+          return div.src; //bot give collection of image links
+        });
+      })
+      .end() //doing something new
+      .then(function (result) {
+        return result.slice(0, 6); //cuts down # of images found to 6
+      })
+      .then(function (images) {
+        res.json(images); //gives back images to user
+      })
+      .catch(function (error) {
+        console.error('Search failed:', error); //what to do if theres a failure
+      });
+  }
+
+  findDragonImage(keyword); //actually performs the function
+});
 
 
 app.listen(port, () => {
